1d7e25a36dd09da8788d3a6c574fe4e61e56323d
Justin Tooley <jstooley823@gmail.com>
2017-11-11 23:08:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,5 +19,8 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
-  get "/multiply/:num1/:num2"
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+  end
 end
